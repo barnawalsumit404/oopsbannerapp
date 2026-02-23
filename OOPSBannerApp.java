0c38db5095cb9f0d8a0b5e5f5d55e50e37942b6d
Sumit Barnawar <mailtobarnawal@gmail.com>
2026-02-23 13:32:04 +0530

    UC3: Print OOPS banner using String.join()

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,27 +1,21 @@
 /**
  * OOPSBannerApp
- * UC2: Prints OOPS in Banner Format using * and spaces
- * @author Sumit
- * @version 2.0
+ * UC3: Prints OOPS in Banner Format using String.join()
+ * @author Developer
+ * @version 3.0
  */
 public class OOPSBannerApp {
     public static void main(String[] args) {
         printOOPSBanner();
     }
 
-    // Function to print OOPS in banner format
+    // Function to print OOPS in banner format using String.join()
     public static void printOOPSBanner() {
-        String[] banner = {
-            " ***   ***   ****   **** ",
-            "*   * *   * *    * *     ",
-            "*   * *   * *    * *     ",
-            "*   * *   * ****   **** ",
-            "*   * *   * *          *",
-            " ***   ***  *     ****  "
-        };
-
-        for (String line : banner) {
-            System.out.println(line);
-        }
+        System.out.println(String.join("", "   **     **   ******   ****** "));
+        System.out.println(String.join("", " **  ** **  ** **    ** **     "));
+        System.out.println(String.join("", " **  ** **  ** **    ** **     "));
+        System.out.println(String.join("", " **  ** **  ** ******   ****** "));
+        System.out.println(String.join("", " **  ** **  ** **           **  "));
+        System.out.println(String.join("", "   **     **   **      *******  "));
     }
 }
